a8c16fbb093473c1ea4ac3b767ff4a1555b65693
JB <jacobbunnellca@gmail.com>
2022-08-04 11:47:20 -0400

    Created function for randomized choice from computer

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello World!');
+//console.log('Hello World!');
+
+//get computer to make random choice 
+function getCompChoice(arr) {
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    // get random item
+    const item = arr[randomIndex];
+    // return choice from array
+    return item;
+}
+//create array of Rock, Paper or scissors
+const choices = ['Rock', 'Paper', 'Scissors'];
+//get result of random computer selection
+const result = getCompChoice(choices);
+//prints result 
+console.log(result);
+
+
+
